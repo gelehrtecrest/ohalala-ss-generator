e4050cc92f70a2be8d8081ea5b51c0539ee0bc50
Gelehrte Crest <ein.gelehrte@gmail.com>
2017-03-01 19:40:09 +0900

    他のロゴも貼れるように

--- docs/main.js
+++ docs/main.js
@@ -8,7 +8,7 @@
 	function loadImage (imageData){
 		//画像のロード
 		var baseImg = new Image();
-		baseImg.src = 'https://pbs.twimg.com/media/C2CtwVgUsAAaz86.png';
+		baseImg.src = $('#logourl').val();
 		img = new createjs.Bitmap(baseImg);
 
 		//画像が選択されている時のみ合成
@@ -44,6 +44,22 @@
 	}
 
 	$(function(){
+		//設定のデフォルト値
+		$('#logourl').val('https://pbs.twimg.com/media/C2CtwVgUsAAaz86.png');
+
+		$('#logourl').keyup(function() {
+			$.ajax({
+				url: $('#logourl').val()
+			}).done(function(data){
+				var baseImg = new Image();
+				baseImg.src = $('#logourl').val();
+				img = new createjs.Bitmap(baseImg);
+				$("#alert").text("");
+			}).fail(function(data){
+				$("#alert").text("ロゴのURLが間違っています。ヒント：httpsから始まるURLにしてください。");
+			});
+		});
+
 		//読込画像のオブジェクト
 		var imageIni = {
 			xPos : 2,
@@ -51,9 +67,9 @@
 			Scale : -5,
 			imageData : null,
 			resetImage : function(){
-				this.xPos = 0;
-				this.yPos = 0;
-				this.Scale = 0;
+				this.xPos = 2;
+				this.yPos = 2;
+				this.Scale = -5;
 			},
 			makeImage : function(){
 				if(this.imageData !== null) {
@@ -87,7 +103,6 @@
 		//ボタンイベントまとめ
 		$('.btn').on('click',function(e){
 			if (e.target.id === "update"){
-				//画像生成は個別処理なし
 			}else if (e.target.id === "up"){
 				imageIni.yPos -= 1;
 			}else if (e.target.id === "down"){
@@ -109,7 +124,6 @@
 			//画像操作時は再描画を行う
 			if(imageIni.imageData !== null){
 				imageIni.makeImage();
-				$("#alert").text("");
 			}else{
 				$("#alert").text("スクリーンショットを入力してから画像生成を行ってください");
 			}
